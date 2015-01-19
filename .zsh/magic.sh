176ae6b3386b93127154082faa424f9bf208a313
Marie Markwell <me@marie.so>
2015-01-19 04:53:52 -0500

    Update magic.sh.

--- .zsh/magic.sh
+++ .zsh/magic.sh
@@ -1,6 +1,6 @@
 # Stolen from http://da.gd/lazyJSON
 function -lazyJSON {
-	print -r -- $(grep -Po "(?<=\"$1\":\")(\\\\.|[^\"])+" <<< $response)
+	print -r -- $(grep -Po "(?<=\"$2\":\")(\\\\.|[^\"])+" <<< "$1")
 }
 
 function accept-line() {
@@ -14,25 +14,28 @@ function accept-line() {
 		BUFFER="sicuro ${(qq)tmp}"
 	elif [[ "${${=BUFFER}[1]}" =~ [0-9]+.?[0-9]* ]]; then
 		BUFFER="dc -e ${(qq)BUFFER}' f'"
-	#elif [[ "${${=BUFFER}[1]}" =~ ![^>]*>> ]]; then
+	#elif [[ "${${=BUFFER}[1]}" =~ \![^\>]*\>\>[\s]*(.*)$ ]]; then
 		# !X>> where X is the Ruby version.
-		#...
+	#	BUFFER="!ruby> ${BUFFER#*>}"
 	elif [[ "${${=BUFFER}[1]}" =~ \!([^\>]+)\>[\s]*(.*) ]]; then
 		language=${${BUFFER%>*}:1}
 		code=${BUFFER#*>}
+
 		echo
-		request='{"language":'"${(qqq)language}"',"code":'"${(qqq)code}"'}'
-		command="curl -sS -d ${(qq)request} http://beta.eval.so/api/evaluate"
-		response=$(eval $command)
-		stderr=$(-lazyJSON stderr)
-		stdout=$(-lazyJSON stdout)
+
+		response=$(ruby -revalso -e 'puts Evalso.run(language: ARGV[0], code: ARGV[1]).to_json' -- "$language" "$code")
+		#request='{"language":'"${(qqq)language}"',"code":'"${(qqq)code}"'}'
+		#command="curl -sS -d ${(qq)request} http://beta.eval.so/api/evaluate"
+		#response=$(eval $command)
+		stderr=$(-lazyJSON "$response" stderr)
+		stdout=$(-lazyJSON "$response" stdout)
 
 		if [[ -n "$stderr" ]]; then
 			echo "STDERR:"
 			echo $stderr
 		fi
 		echo "STDOUT:"
-		echo -n $(-lazyJSON stdout)
+		echo -n $stdout
 		BUFFER=""
 	fi
 
