23a58176b03333b40b1ca712cbb801606a9cc612
Marie Markwell <me@marie.so>
2015-03-12 16:51:57 -0400

    Fix ripl.sh.

--- .zsh/ripl.sh
+++ .zsh/ripl.sh
@@ -1,3 +1,3 @@
 if ! which ripl &>/dev/null; then
-  alias ripl="echo 'Installing ripl...' && sed 's/%w\[\(.*\)\].*/\1/' ~/.riplrc | sed 's/^\| / ripl-/g' | xargs gem install && unalias ripl && echo 'Done! Executing ripl as normal.' && ripl"
+  alias ripl="echo 'Installing ripl...' && sed 's/%w\[\(.*\)\].*/\1/' ~/.riplrc | sed 's/^\| / ripl-/g' | xargs gem install && unalias ripl && echo 'Done! Executing ripl as normal.' && \"$@\""
 fi
