263ed9b503e7eec8bb53bb44821c028e0877832e
Marie Markwell <me@marie.so>
2015-08-11 15:21:07 -0400

    Make vim-plug.sh work on systems with chruby.

--- vim-plug.sh
+++ vim-plug.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+[ -f "/usr/local/share/chruby/chruby.sh" ] && source /usr/local/share/chruby/chruby.sh
+[ -n "$CHRUBY_VERSION" ] && chruby system
+
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugUpdate -c qa
