24207dd25282f4e6165a2c22c5a07b1422c86602
Ellen Marie Dash <me@duckie.co>
2017-11-23 10:02:31 -0500

    more cleanup!

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -5,11 +5,13 @@
 
 [ -f "$HOME/.zshenv.user" ] && source $HOME/.zshenv.user
 
-# Defaults
-[ -z "$EDITOR_CHOICES"     ] && EDITOR_CHOICES=(vim nano)
-[ -z "$GUI_EDITOR_CHOICES" ] && GUI_EDITOR_CHOICES=(gedit geany gvim)
-[ -z "$PAGER_CHOICES"      ] && PAGER_CHOICES=(less more most)
-[ -z "$BROWSER_CHOICES"    ] && BROWSER_CHOICES=(opera firefox chromium-browser chrome midori chromium) # Chromium is last because sometimes it's a game.
+export EDITOR=vim
+if ! $(which vim &>/dev/null); then
+  export EDITOR=nano
+fi
+
+export PAGER=less
+export BROWSER=firefox
 
 # -R: Allow ANSI escape codes by default. Needed for $RI below.
 # -F: Automagically exit if the file fits on one screen.
@@ -19,51 +21,3 @@ export LESS="-RFX"
 
 # -f ansi: Use ANSI escape codes by default.
 export RI="-f ansi"
-
-# Make RHEL, CentOS, and NetBSD(?) behave themselves.
-# Run `touch ~/.zsh-is-old-as-crap` to use this.
-# TODO: Make this less hacky.
-if [ ! -f "~/.zsh-is-old-as-crap" ]; then
-
-# Yes, this is painfully repetitive.
-# I'm working on a way to fix that...
-
-if [ -z "$EDITOR" ]; then
-	for ((i=0;i<${#EDITOR_CHOICES};i++)); do
-		if $(exists ${EDITOR_CHOICES:$i:1}); then
-			export EDITOR=${EDITOR_CHOICES:$i:1}
-			break
-		fi
-	done
-fi
-
-if [ -z "$GUI_EDITOR" ]; then
-	for ((i=0;i<${#GUI_EDITOR_CHOICES};i++)); do
-		if $(exists ${GUI_EDITOR_CHOICES:$i:1}); then
-			export GUI_EDITOR=${GUI_EDITOR_CHOICES:$i:1}
-			break
-		fi
-	done
-fi
-
-if [ -z "$PAGER" ]; then
-	for ((i=0;i<${#PAGER_CHOICES};i++)); do
-		if $(exists ${PAGER_CHOICES:$i:1}); then
-			export PAGER=${PAGER_CHOICES:$i:1}
-			break
-		fi
-	done
-fi
-
-if [ -n "$DISPLAY" ] && [ -z "$BROWSER" ]; then
-	for ((i=0;i<${#BROWSER_CHOICES};i++)); do
-		if $(exists ${BROWSER_CHOICES:$i:1}); then
-			export BROWSER=${BROWSER_CHOICES:$i:1}
-			break
-		fi
-	done
-fi
-
-
-fi
-
--- .zsh/path.sh
+++ .zsh/path.sh
@@ -1,27 +1,10 @@
-# TODO: Make this not barf in my $PATH
-[ -d "/etc/profile.d/*.sh" ] && source /etc/profile.d/*.sh
-
-tryppath $HOME/bin
-
-tryppath $HOME/go/bin
-
-tryppath $HOME/.cabal/bin
-
-# https://docs.npmjs.com/getting-started/fixing-npm-permissions
-tryppath $HOME/.npm-global/bin
-
-tryppath /usr/local/heroku/bin
-
+RUBY_PATHS=""
 if [ -d $HOME/.gem/ruby ]; then
   for dir in $HOME/.gem/ruby/*; do
-    tryapath $dir/bin
+    RUBY_PATHS="$dir/bin:$RUBY_PATHS"
   done
 fi
 
-# For when using the GHC PPA. https://launchpad.net/~hvr/+archive/ubuntu/ghc
-if [ -n "$(find /opt/ghc -maxdepth 1 2>/dev/null)" ]; then
-  __GHC_VERSION=$(find /opt/ghc/ -maxdepth 1 | cut -d '/' -f 4 | sort -n | tail -n1)
-  ppath "/opt/ghc/${__GHC_VERSION}/bin"
-  unset __GHC_VERSION
-fi
-
+# Re: .npm-global: https://docs.npmjs.com/getting-started/fixing-npm-permissions
+export PATH="$HOME/bin:$HOME/go/bin:$HOME/.cabal/bin:$HOME/.npm-global/bin:$RUBY_PATHS:$PATH"
+unset RUBY_PATHS
