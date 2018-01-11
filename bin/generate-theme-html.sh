b71f97c88cf24eb4f3ed3d62e9decd3b7edfa04b
Ellen Marie Dash <me@duckie.co>
2018-01-11 18:28:20 -0500

    tweak generate-theme-html.sh.

--- bin/generate-theme-html.sh
+++ bin/generate-theme-html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "<!DOCTYPE html>" > theme.html; echo "<style>p{margin:0;}</style>" >> theme.html; cat data/theme.js | grep '#' | cut -d '"' -f 2 | sed 's/\(.*\)/<p style="background-color: \1;">\1<\/p>/' >> theme.html
+echo "<!DOCTYPE html>" > theme.html; echo "<style>p{margin:0;font: 22pt awoof;}</style>" >> theme.html; cat data/theme.js | grep '#' | cut -d '"' -f 2 | sed 's/\(.*\)/<p style="background-color: \1;">\1<\/p>/' >> theme.html
