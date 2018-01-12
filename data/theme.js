6559cec6f95d77d8e6cd9cf257554cb5d9c70466
Ellen Marie Dash <me@duckie.co>
2018-01-11 20:57:13 -0500

    more theme documentation. :3

--- data/theme.js
+++ data/theme.js
@@ -1,16 +1,26 @@
 exports.colors = {
   dark: {
     accent0: "#777777",
+
+    /* vim — ??? */
     accent1: "#CE95CE",
+
+    /* vim — identifiers */
     accent2: "#BADBDB",
-    accent3: "#E2E7E6",
+
+    /* vim — constants and symbols in the "gutter" for git stuff */
+    accent3:  "#E2E7E6",
+
+    /* vim — vim UI stuff, I think */
     accent4: "#E3BFBD",
+
+    /* vim — keywords (if, else, return, etc.)  */
     accent5: "#C85CC8",
 
-    /* vim: preprocessor stuff (e.g. #include in C.) */
+    /* vim — preprocessor stuff (e.g. #include in C.) */
     accent6: "#9ABAEA",
 
-    /* vim: types */
+    /* vim — types */
     accent7: "#FFFFFF",
 
     /* === */
@@ -19,19 +29,19 @@ exports.colors = {
     shade1: "#4E244E",
     shade2: "#B9DADA",
 
-    /* vim: line numbers. */
+    /* vim — line numbers. */
     shade3: "#E0E6E5",
 
-    /* vim: modeline. */
+    /* vim — modeline. */
     shade4: "#EAD0CE",
 
-    /* vim: ??? */
+    /* vim — ??? */
     shade5: "#AA00AA",
 
-    /* vim: default text color. */
+    /* vim — default text color. */
     shade6: "#6194DF",
 
-    /* vim: error messages and questions(???) */
+    /* vim — error messages and questions(???) */
     shade7: "#F7F9F9",
   },
 };
