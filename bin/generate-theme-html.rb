f043cf85151cc313c923f9a645ebe69c8395a572
Ellen Marie Dash <me@duckie.co>
2018-04-16 17:44:30 -0400

    simplify script.

--- bin/generate-theme-html.rb
+++ bin/generate-theme-html.rb
@@ -6,7 +6,7 @@ doctype = "<!doctype html>"
 css = "<style>p{margin: 0;padding: 0.25em;font: 20pt Monospace;}</style>"
 colors =
   File.read('data/color-scheme.txt') \
-    .split("\n")    \
+    .lines          \
     .grep(/#/)      \
     .map(&:split)   \
     .map(&to_html)  \
