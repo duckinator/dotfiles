a3139c0bf2bdd81452283989b9153dc5744ee037
Ellen Marie Dash <me@duckie.co>
2018-04-21 16:32:10 -0400

    Improve bin/dotfiles-bootstrap.py script.

--- bin/dotfiles-bootstrap.py
+++ bin/dotfiles-bootstrap.py
@@ -2,27 +2,21 @@
 
 import os
 import subprocess
-import sys
+from pathlib import Path
 
-home_dir = os.environ["HOME"]
-dotfiles_dir = os.path.join(home_dir, "dotfiles")
-clone_command = [
-    "git", "clone", "https://github.com/duckinator/dotfiles.git", dotfiles_dir
-]
+dotfiles_dir = Path(Path.home(), "dotfiles")
+repo_url = "https://github.com/duckinator/dotfiles.git"
+
+def run(command):
+    results = subprocess.check_output(command, encoding="utf-8")
+    print("    $ {}\n{}".format(" ".join(command), results))
 
 if not os.path.isdir(dotfiles_dir):
-    os.chdir(home_dir)
-    results = subprocess.check_output(clone_command, encoding="utf-8")
+    run(["git", "clone", repo_url, dotfiles_dir])
 
 os.chdir(dotfiles_dir)
 
-def run(cmd):
-    return "[{}] $ {}\n{}".format(
-            sys.argv[0],
-            cmd,
-            subprocess.check_output(cmd.split(), encoding="utf-8"))
-
-print(run("gem install -r effuse"))
-print(run("effuse -c"))
-print(run("git pull"))
-print(run("effuse"))
+run(["gem", "install", "-r", "effuse"])
+run(["effuse", "-c"])
+run(["git", "pull"])
+run(["effuse"])
