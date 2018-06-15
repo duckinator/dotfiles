08ecd2693d1c7e3e3ca8a6dc7dbd0dae57ae3551
Ellen Marie Dash <me@duckie.co>
2018-06-15 10:55:33 -0400

    Clean up bootstrap.py.

--- bootstrap.py
+++ bootstrap.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python3
 
 import os
-from pip._internal import main as pip_main
-import stat
-import subprocess
+from pip._internal import main as pip
+from subprocess import check_output
 import sys
 
-def print_run(command):
-    print("$ {}".format(" ".join(command)))
+def run(fn, *args, name=None):
+    print("$ {} {}".format(name or fn.__name__, " ".join(args)))
+    results = fn(list(args))
+    if isinstance(results, str):
+        print(results, end='', flush=True)
 
-def run(command):
-    print_run(command)
-    results = subprocess.check_output(command, encoding="utf-8").strip()
-    if len(results) > 0:
-        print(results)
-
-def git(*args):
-    run(["git", *args])
-
-def pip(*args):
-    print_run(["pip", *args])
-    pip_main([*args, "--user", "--no-cache-dir", "--quiet"])
-
-def emanate(*args):
-    import emanate
-    print_run(["emanate", *args])
-    emanate.main(args)
+def git(args):
+    return check_output(["git", *args], encoding="utf-8")
 
 def main():
     dotfiles_dir = os.path.dirname(__file__)
     os.chdir(dotfiles_dir)
-    pip("install", "emanate")
-    #emanate("--clean")
-    git("pull")
-    emanate()
+    run(pip, "install", "emanate", "--user", "--no-cache-dir",
+            name="pip")
+    import emanate
+
+    #run(emanate, "--clean")
+    run(git, "pull")
+    run(emanate.main,
+            name="emanate")
 
 if __name__ == "__main__":
     main()
