736171aabb977e6d9e47f03d33f0f213776274a9
Ellen Marie Dash <me@duckie.co>
2018-06-15 11:19:10 -0400

    clean up bootstrap.py.

--- bootstrap.py
+++ bootstrap.py
@@ -4,9 +4,9 @@ import os
 from pip._internal import main as pip
 from subprocess import check_call
 
-def run(fn, *args, name):
+def run(name, args, fn):
     print("$ {} {}".format(name, " ".join(args)))
-    return fn(list(args))
+    fn(args)
 
 def git(args):
     return check_call(["git", *args])
@@ -14,16 +14,16 @@ def git(args):
 def main():
     dotfiles_dir = os.path.dirname(__file__)
     os.chdir(dotfiles_dir)
-    run(pip, "install", "emanate", "--user", "--no-cache-dir",
-            name="pip")
+    run("pip", ["install", "emanate", "--user", "--no-cache-dir"],
+            fn=pip)
     import emanate
 
-    #run(emanate.main, "--clean",
-    #        name="emanate")
-    run(git, "pull",
-            name="git")
-    run(emanate.main,
-            name="emanate")
+    #run("emanate", ["--clean"],
+    #        fn=emanate.main)
+    run("git", ["pull"],
+            fn=git)
+    run("emanate", [],
+            fn=emanate.main)
 
 if __name__ == "__main__":
     main()
