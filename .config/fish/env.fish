e04c51f2763f7f34b47cbb3464a769845be5351c
Ellen Marie Dash <me@duckie.co>
2018-07-19 07:34:26 -0400

    Move sourcing of ~/.cargo/env to after $PATH is set.

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -5,12 +5,12 @@ if test -d "/var/run/user/$UID"
   export XDG_RUNTIME_DIR="/var/run/user/$UID"
 end
 
+# Re: .npm-global: https://docs.npmjs.com/getting-started/fixing-npm-permissions
+set -gx PATH "$HOME/bin" "$HOME/.local/bin" "$HOME/.gem/ruby/bin" "$HOME/.npm-global/bin" "/bin" "/usr/bin" "/sbin" "/usr/sbin"
+
 if test -f ~/.cargo/env
   source ~/.cargo/env
 end
-
-# Re: .npm-global: https://docs.npmjs.com/getting-started/fixing-npm-permissions
-set -gx PATH "$HOME/bin" "$HOME/.local/bin" "$HOME/.gem/ruby/bin" "$HOME/.npm-global/bin" "/bin" "/usr/bin" "/sbin" "/usr/sbin"
 
 if which nvim >/dev/null ^/dev/null
   export EDITOR=nvim
