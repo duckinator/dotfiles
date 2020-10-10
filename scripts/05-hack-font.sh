3b556e42eeeed8c0e839791bb3c4b6652f80fb55
Ellen Marie Dash <me@duckie.co>
2020-10-10 01:38:02 -0400

    Add scripts/05-hack-font.sh.

---
+++ scripts/05-hack-font.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FONTDIR="$HOME/.fonts"
+
+if [ "$1" = "create" ]; then
+    mkdir -p "$FONTDIR"
+
+    TMPDIR="$(mktemp -d)"
+    cd "$TMPDIR"
+    wget -O hack.zip https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
+    unzip hack.zip
+    mv ttf/*.ttf "$FONTDIR"
+
+    cd
+    rm -rf "$TMPDIR"
+fi
+
+if [ "$1" = "clean" ]; then
+    [ -d "$FONTDIR" ] || exit
+    cd "$FONTDIR"
+    rm -f Hack-*.ttf
+fi
