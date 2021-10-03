dd91285ed7eb81fdb1f12aa9742f07b197360148
Ellen Marie Dash <me@duckie.co>
2021-10-03 00:10:28 -0400

    [bin/*] Use /bin/sh or /usr/bin/env bash, as appropriate.

--- bin/_generate-beep.sh
+++ bin/_generate-beep.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rec -q ~/dotfiles/.sounds/beep.wav channels 2 rate 16k synth 0.1 sq 750 vol 0.1
--- bin/check-battery.sh
+++ bin/check-battery.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 upower -i $(upower -e | grep -i battery)
