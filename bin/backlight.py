28374c6cf998fba3528e8a15adba3a4e327cc2d1
Ellen Marie Dash <me@duckie.co>
2024-10-11 21:06:09 -0400

    update bin/backlight.py

--- bin/backlight.py
+++ bin/backlight.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+from subprocess import check_call
 
-device = "nv_backlight"
-device_path = "/sys/class/backlight/" + device
+num_displays = 2
+
+def backlight_set(level):
+    # sudo ddcutil --display 1 setvcp 10 50
+    for display in range(1, num_displays + 1):
+        check_call(["sudo", "ddcutil", "--display", str(display), "setvcp", "10", str(level)])
 
 
-def backlight_set(file_path, level):
-    with open(file_path, "w") as f:
-        f.write(str(level))
-
-
-def backlight_get(file_path):
-    with open(file_path) as f:
-        return int(f.read())
+def backlight_get():
+    # sudo ddcutil --display 1 getvcp 10 50
+    for display in range(1, num_displays + 1):
+        results = check_call(["ddcutil", "--display", str(display), "getvcp", "10"])
+        print(results)
 
 
 def backlight_calc(current, min_, max_, string):
@@ -31,20 +33,17 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    cur_file = device_path + "/brightness"
-    max_file = device_path + "/max_brightness"
+    #cur_, max_ = backlight_get()
+    #min_ = 1
+    #if len(argv) < 2:
+    #    print("{} / {}".format(cur_, max_))
+    #    return 0
 
-    cur_ = backlight_get(cur_file)
-    min_ = 1
-    max_ = backlight_get(max_file)
-    if len(argv) < 2:
-        print("{} / {}".format(cur_, max_))
-        return 0
+    #brightness = backlight_calc(cur_, min_, max_, argv[1])
 
-    brightness = backlight_calc(cur_, min_, max_, argv[1])
-
-    print(brightness)
-    backlight_set(cur_file, brightness)
+    #print(brightness)
+    brightness = argv[1]
+    backlight_set(brightness)
 
     return 0
 
